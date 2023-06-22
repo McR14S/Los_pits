7147180a65593d06a8eea6799e00ce9ebbec62d1
McR14S <dan.arancibiar@duocuc.cl>
2023-06-22 01:14:38 -0400

    hotfix stocks

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -61,7 +61,7 @@ class ProductsController < ApplicationController
     private
 
     def product_params
-        params.require(:product).permit(:title, :description, :price, :photo, :category_id)
+        params.require(:product).permit(:title, :description, :price, :photo, :stock ,:category_id)
     end
 
     #Limpiamos parametros no especificados
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_22_044008) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_22_045926) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_22_044008) do
     t.datetime "updated_at", null: false
     t.bigint "category_id", null: false
     t.bigint "user_id", null: false
+    t.integer "stock"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
